1efb48797e390d8bf0b5217eae93e78b67f6c0b2
FredyMV <FredyMV>
2016-05-17 08:36:41 -0500

    Capturando el evento click en los charts!

--- controller/statusController.js
+++ controller/statusController.js
@@ -1,218 +1,165 @@
 (function () {
 
-	var controllerModule = angular.module('AppControllers');
+		var controllerModule = angular.module('AppControllers');
 
-	controllerModule.controller('statusController', ['$scope','$rootScope', '$location', 'userService', 'userFactory',
-													 'balanceChartFactory', 'patrimonioUserFactory',
-		function ($scope, $rootScope, $location, userService, userFactory, balanceChartFactory,patrimonioUserFactory
-		) {
-			// Id del usuario Autenticado.
-			var _idUser = $rootScope.idUserLoggedIn;
+		controllerModule.controller('statusController', ['$scope', '$rootScope', '$location', '_', 'userService', 'userFactory'
 
-			//Obtener el patrimonio del usuario.
-			userService.getEdificioUsuario(_idUser).then(function (response){
-				//edificiosUserFactory.setEdificioData(response.data);
-				var edificios = response.data;
-				userService.getPatrimonioEdificio(_idUser).then(function (response){
-					patrimonioUserFactory.setPatrimonioData(response.data, edificios);
-					$scope.patrimonioUser = patrimonioUserFactory;
-					console.log($scope.patrimonioUser);
-					paintDoughnutToBarrioFlexibilidad($scope.patrimonioUser.flexibilidad);
-					paintDoughnutToBarrioColaboracion($scope.patrimonioUser.colaboracion);
-					paintDoughnutToBarrioInnovacion($scope.patrimonioUser.innovacion);
-				});
-			}, function (error){
-				$location.path('/home');
-			});
+				, 'balanceChartFactory', 'patrimonioUserFactory'
 
-			//Obtener la informacion del usuario.
-			userService.getInfoUser(_idUser).then(function (response) {
-				userFactory.setUserData(response.data);
-				$scope.user = userFactory;
-				$scope.user.saludObj = userFactory.getSalud();
-				paintSliderChart($scope.user.saludObj); //Pintar Slider de la salud del usuario.
-			}, function (error) {
-				$location.path('/home');
-			});
+				, function ($scope, $rootScope, $location, _, userService, userFactory, balanceChartFactory, patrimonioUserFactory) {
+					// Id del usuario Autenticado.
+					var _idUser = $rootScope.idUserLoggedIn;
 
-			//Obtener el balance del usuario.
-			userService.getBalance(_idUser).then(function (response) {
-				balanceChartFactory.runProcess(response.data);
-				paintChartCash(balanceChartFactory);
-				paintChartUnidadOro(balanceChartFactory);
-			}, function (error) {
-				$location.path('/home');
-			});
+					//Obtener el patrimonio del usuario.
+					userService.getEdificioUsuario(_idUser).then(function (response) {
+						var edificios = response.data;
+						userService.getPatrimonioEdificio(_idUser).then(function (response) {
+							patrimonioUserFactory.setPatrimonioData(response.data, edificios);
+							$scope.patrimonioUser = patrimonioUserFactory;
+							var dataChart =
+								$scope.patrimonioUser.convertToDoughnutChartDataFormat($scope.patrimonioUser.flexibilidad);
+							showDonoutCharFlexibilidad(dataChart);
+							dataChart =
+								$scope.patrimonioUser.convertToDoughnutChartDataFormat($scope.patrimonioUser.colaboracion);
+							showDonoutCharColaboracion(dataChart);
+							dataChart =
+								$scope.patrimonioUser.convertToDoughnutChartDataFormat($scope.patrimonioUser.innovacion);
+							showDonoutCharInnovacion(dataChart);
 
-			$scope.getStyle = function () {
-				var transform = ($scope.isSemi ? '' : 'translateY(50%) ') + 'translateX(-50%)';
-				return {
-					'top': $scope.isSemi ? 'auto' : '50%'
-					, 'bottom': $scope.isSemi ? '5%' : 'auto'
-					, 'left': '50%'
-					, 'transform': transform
-					, '-moz-transform': transform
-					, '-webkit-transform': transform
-					, 'font-size': $scope.radius / 3.5 + 'px'
-				};
-			};
-
-			/************** Charts Functions ********************/
-			var showDonoutChar = function () {
-				$scope.labels_barrio = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
-				$scope.data_barrio = [300, 500, 100];
-				$scope.onClick = function (points, evt) {
-					//console.log(points, evt);
-				};
-			}
-
-			var paintSliderChart = function (salud) {
-				var saludInt = parseInt(salud.value);
-				$scope.sliderSalud = {
-					value: saludInt
-					, options: {
-						floor: 0
-						, ceil: 100
-						, readOnly: true
-						, showTicksValues: 25
-						, showSelectionBar: true
-						, getSelectionBarColor: function () {
-							return salud.color
-						}
-						, getPointerColor: function () {
-							return salud.color
-						}
-					}
-				};
-			}
-
-			var paintChartCash = function (balance) {
-				$scope.labels_cash = balance.getLabelsCash();
-				$scope.series_cash = balance.getSeriesCash();
-				$scope.data_cash = balance.getDataCash();
-				$scope.canvas_cash_show = $scope.labels_cash.length != 0 ? true : false;
-			}
-
-			var paintChartUnidadOro = function (balance) {
-				$scope.labels_unid_oro = balance.getLabelsUnidadOro();
-				$scope.series_unid_oro = balance.getSeriesUnidadOro();
-				$scope.data_unid_oro = balance.getDataUnidadOro();
-				$scope.canvas_unid_oro_show = $scope.labels_unid_oro.length != 0 ? true : false;
-			}
-
-			var paintDoughnutToBarrioFlexibilidad = function (barrioObject){
-
-				$scope.labels_flexibilidad = [];
-				$scope.data_flexibilidad = [];
-
-				if(barrioObject.edificios ==0){
-					$scope.labels_flexibilidad.push("Ed. No construido");
-					$scope.data_flexibilidad.push(100);
-				}
-				else{
-					var num_porcent_barrio = 100/parseInt(barrioObject.propiedades_maximo);
-					if(barrioObject.propiedades_construidas != 0 &&
-					   barrioObject.propiedades_construidas < barrioObject.propiedades_maximo)
-					{
-						barrioObject.edificios.forEach(function (item, index) {
-								$scope.labels_flexibilidad.push(item.info);
-								$scope.data_flexibilidad.push(num_porcent_barrio);
 						});
-						var dif = barrioObject.propiedades_maximo - barrioObject.propiedades_construidas;
-						for(var i=0;i<dif;i++){
-							$scope.labels_flexibilidad.push("Ed. No construido");
-							$scope.data_flexibilidad.push(num_porcent_barrio);
-						}
+					}, function (error) {
+						$location.path('/home');
+					});
 
+					//Obtener la informacion del usuario.
+					userService.getInfoUser(_idUser).then(function (response) {
+						userFactory.setUserData(response.data);
+						$scope.user = userFactory;
+						$scope.user.saludObj = userFactory.getSalud();
+						paintSliderChart($scope.user.saludObj); //Pintar Slider de la salud del usuario.
+					}, function (error) {
+						$location.path('/home');
+					});
+
+					//Obtener el balance del usuario.
+					userService.getBalance(_idUser).then(function (response) {
+						balanceChartFactory.runProcess(response.data);
+						paintChartCash(balanceChartFactory);
+						paintChartUnidadOro(balanceChartFactory);
+					}, function (error) {
+						$location.path('/home');
+					});
+
+					$scope.getStyle = function () {
+						var transform = ($scope.isSemi ? '' : 'translateY(50%) ') + 'translateX(-50%)';
+						return {
+							'top': $scope.isSemi ? 'auto' : '50%'
+							, 'bottom': $scope.isSemi ? '5%' : 'auto'
+							, 'left': '50%'
+							, 'transform': transform
+							, '-moz-transform': transform
+							, '-webkit-transform': transform
+							, 'font-size': $scope.radius / 3.5 + 'px'
+						};
+					};
+					/************** Charts Functions ********************/
+					var paintSliderChart = function (salud) {
+						var saludInt = parseInt(salud.value);
+						$scope.sliderSalud = {
+							value: saludInt
+							, options: {
+								floor: 0
+								, ceil: 100
+								, readOnly: true
+								, showTicksValues: 25
+								, showSelectionBar: true
+								, getSelectionBarColor: function () {
+									return salud.color
+								}
+								, getPointerColor: function () {
+									return salud.color
+								}
+							}
+						};
 					}
-					else
-					{
-						barrioObject.edificios.forEach(function (item, index) {
-							$scope.labels_flexibilidad.push(item.info);
-							$scope.data_flexibilidad.push(item.precio);
-						});
+					var showDonoutCharFlexibilidad = function (dataChart) {
+						$scope.labels_flexibilidad = dataChart.label;
+						$scope.data_flexibilidad = dataChart.data;
+						$scope.onClickFlexibilidad = function (points, evt) {
+							var barrio = $scope.patrimonioUser.flexibilidad
+							showDetalleEdificioEvent(points, barrio.nombre_barrio, barrio.edificios);
+						};
 					}
-				}
-
-			}
-
-			var paintDoughnutToBarrioColaboracion = function (barrioObject){
-
-				$scope.labels_Colaboracion = []
-				$scope.data_Colaboracion = []
-
-				if(barrioObject.edificios ==0){
-					$scope.labels_Colaboracion.push("Ed. No construido");
-					$scope.data_Colaboracion.push(100);
-				}
-				else{
-					var num_porcent_barrio = 100/parseInt(barrioObject.propiedades_maximo);
-					if(barrioObject.propiedades_construidas != 0 &&
-					   barrioObject.propiedades_construidas < barrioObject.propiedades_maximo)
-					{
-						barrioObject.edificios.forEach(function (item, index) {
-								$scope.labels_Colaboracion.push(item.info);
-								$scope.data_Colaboracion.push(num_porcent_barrio);
-						});
-						var dif = barrioObject.propiedades_maximo - barrioObject.propiedades_construidas;
-						for(var i=0;i<dif;i++){
-							$scope.labels_Colaboracion.push("Ed. No construido");
-							$scope.data_Colaboracion.push(num_porcent_barrio);
-						}
-
+					var showDonoutCharColaboracion = function (dataChart) {
+						$scope.labels_Colaboracion = dataChart.label
+						$scope.data_Colaboracion = dataChart.data
+						$scope.onClickColaboracion = function (points, evt) {
+							var barrio = $scope.patrimonioUser.colaboracion
+							showDetalleEdificioEvent(points, barrio.nombre_barrio, barrio.edificios);
+						};
 					}
-					else
-					{
-						barrioObject.edificios.forEach(function (item, index) {
-							$scope.labels_Colaboracion.push(item.info);
-							$scope.data_Colaboracion.push(item.precio);
-						});
+					var showDonoutCharInnovacion = function (dataChart) {
+						$scope.labels_Innovacion = dataChart.label;
+						$scope.data_Innovacion = dataChart.data
+						$scope.onClickInnovacion = function (points, evt) {
+							var barrio = $scope.patrimonioUser.innovacion
+							showDetalleEdificioEvent(points, barrio.nombre_barrio, barrio.edificios);
+						};
 					}
-				}
+					var showDetalleEdificioEvent = function(points, nombreBarrio, edificios){
+						if (points[0].label === "NO-CONSTRUIDO") {
+								alert(
+									"Nombre del Barrio: " + nombreBarrio + "\n" + "\n" +
+									"EDIFICIO(S) NO CONSTRUIDO: Te falta el " + points[0].value + "% para terminar"
+								);
+							}else {
+								var edificio = _.find(edificios, function (o) { return o.etiqueta == points[0].label;});
+								alert(
+									"Nombre del Barrio: " + nombreBarrio + "\n" + "\n" +
+									"Etiqueta: " +  edificio.etiqueta + "\n" +
+									"Info: " +  edificio.info + "\n" +
+									"Precio: " +  edificio.precio + "\n" +
+									"Imagen: " +  edificio.imagen + "\n" +
+									"Mejoras (Arreglo): " +  edificio.mejoras + "\n"
+								);
 
-			}
+								/*$uibModal.open({
+									templateUrl: 'addToDoModal'
+									, controller: 'statusController.addToDoModal'
+									, size: 'md'
+								}).result.then(function (t) {
 
-			var paintDoughnutToBarrioInnovacion = function (barrioObject){
-
-				$scope.labels_Innovacion = []
-				$scope.data_Innovacion = []
-
-				if(barrioObject.edificios ==0){
-					$scope.labels_Innovacion.push("Ed. No construido");
-					$scope.data_Innovacion.push(100);
-				}
-				else{
-					var num_porcent_barrio = 100/parseInt(barrioObject.propiedades_maximo);
-					if(barrioObject.propiedades_construidas != 0 &&
-					   barrioObject.propiedades_construidas < barrioObject.propiedades_maximo)
-					{
-						barrioObject.edificios.forEach(function (item, index) {
-								$scope.labels_Innovacion.push(item.info);
-								$scope.data_Innovacion.push(num_porcent_barrio);
-						});
-						var dif = barrioObject.propiedades_maximo - barrioObject.propiedades_construidas;
-						for(var i=0;i<dif;i++){
-							$scope.labels_Innovacion.push("Ed. No construido");
-							$scope.data_Innovacion.push(num_porcent_barrio);
-						}
-
+								});*/
+							}
 					}
-					else
-					{
-						barrioObject.edificios.forEach(function (item, index) {
-							$scope.labels_Innovacion.push(item.info);
-							$scope.data_Innovacion.push(item.precio);
-						});
+
+					var paintChartCash = function (balance) {
+						$scope.labels_cash = balance.getLabelsCash();
+						$scope.series_cash = balance.getSeriesCash();
+						$scope.data_cash = balance.getDataCash();
+						$scope.canvas_cash_show = $scope.labels_cash.length != 0 ? true : false;
 					}
-				}
+					var paintChartUnidadOro = function (balance) {
+						$scope.labels_unid_oro = balance.getLabelsUnidadOro();
+						$scope.series_unid_oro = balance.getSeriesUnidadOro();
+						$scope.data_unid_oro = balance.getDataUnidadOro();
+						$scope.canvas_unid_oro_show = $scope.labels_unid_oro.length != 0 ? true : false;
+					}
 
-			}
+					}])
 
+			/*controllerModule.controller('statusController.addToDoModal', ['$scope', '$modalInstance', function ($scope, $modalInstance) {
+		$scope.todo = {};
 
-			//$scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"]; $scope.data = [35, 15, 50];
+		$scope.cancel = function () {
+			$modalInstance.dismiss('cancel');
+		};
 
-		 }])
+		$scope.ok = function () {
 
+			$modalInstance.close($scope.todo);
+		};
+    }]);*/
 
 
 })();
--- services/patrimonioUserFactory.js
+++ services/patrimonioUserFactory.js
@@ -1,8 +1,8 @@
 /*
-* Patrimonio Factory
-* Servicio para calcular y obtener los datos e informacion
-* del patrimonio del usuario
-*/
+ * Patrimonio Factory
+ * Servicio para calcular y obtener los datos e informacion
+ * del patrimonio del usuario
+ */
 (function () {
 	var servicesModule = angular.module('AppServices');
 	servicesModule.factory('patrimonioUserFactory', ['$rootScope', function ($rootScope) {
@@ -24,15 +24,49 @@
 
 		//Metodo que devuelve el nombre completo del usuario
 		patrimonio.getSumatoriaPrecioVivienda = function () {
-			return patrimonio.colaboracion.valor_propiedades_construidas
-				   + patrimonio.flexibilidad.valor_propiedades_construidas
-				   + patrimonio.innovacion.valor_propiedades_construidas;
+			return patrimonio.colaboracion.valor_propiedades_construidas + patrimonio.flexibilidad.valor_propiedades_construidas +
+				patrimonio.innovacion.valor_propiedades_construidas;
 		}
 
-		patrimonio.getSumPropConstruida = function(){
-			return patrimonio.colaboracion.propiedades_construidas
-				   + patrimonio.flexibilidad.propiedades_construidas
-				   + patrimonio.innovacion.propiedades_construidas;
+		patrimonio.getSumPropConstruida = function () {
+			return patrimonio.colaboracion.propiedades_construidas + patrimonio.flexibilidad.propiedades_construidas +
+				patrimonio.innovacion.propiedades_construidas;
+		}
+
+		patrimonio.convertToDoughnutChartDataFormat = function(barrio) {
+			var labels = [];
+			var datas = [];
+			if (barrio.edificios == 0) {
+				labels.push("NO-CONSTRUIDO");
+				datas.push(100);
+			} else {
+				var num_porcent_barrio = 100 / parseInt(barrio.propiedades_maximo);
+				if (barrio.propiedades_construidas != 0 &&
+					barrio.propiedades_construidas < barrio.propiedades_maximo) {
+					barrio.edificios.forEach(function (item, index) {
+						labels.push(item.etiqueta);
+						datas.push(num_porcent_barrio);
+					});
+					var numporc_noconstruido = num_porcent_barrio * barrio.propiedades_construidas;
+					labels.push("NO-CONSTRUIDO");
+					datas.push(numporc_noconstruido);
+					/*var dif = barrioObject.propiedades_maximo - barrioObject.propiedades_construidas;
+					for (var i = 0; i < dif; i++) {
+						$scope.labels_flexibilidad.push("Ed. No construido");
+						$scope.data_flexibilidad.push(num_porcent_barrio);
+					}*/
+
+				} else {
+					barrio.edificios.forEach(function (item, index) {
+						labels.push(item.info);
+						datas.push(item.precio);
+					});
+				}
+			}
+			return  {
+				label: labels,
+				data: datas
+			};
 		}
 
 		return patrimonio; //retorna el objeto Patrimonio{};
