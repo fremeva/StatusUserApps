d7958c918d75d7c6a3749ea902252898d440f39a
FredyMV <FredyMV>
2016-05-23 15:53:33 -0500

    Cambiando los iconos de los servicios por las imagenes enviadas.

--- controller/homeController.js
+++ controller/homeController.js
@@ -5,20 +5,9 @@
 (function () {
 
 	var controllerModule = angular.module('AppControllers');
-	controllerModule.controller('homeController', ['$scope', 'userService', '$rootScope', 'balanceChartFactory'
-		, function ($scope, userService, $rootScope, balanceChartFactory) {
+	controllerModule.controller('homeController', ['$scope', 'userService', '$rootScope'
+		, function ($scope, userService, $rootScope) {
 			// Funcion para controlar el template home.html
-
-			// Id del usuario Autenticado.
-			var _idUser = $rootScope.idUserLoggedIn;
-			userService.getBalance(_idUser).then(function (response) {
-				$scope.balance = response.data;
-				balanceChartFactory.runProcess(response.data);
-				balanceChartFactory.getDataCashPorDia();
-
-			}, function (error) {
-				$location.path('/home');
-			});
 
     }]);
 
